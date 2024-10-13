8ad606da78e5636595b476939d0bed47bf71ae9c
Stanisław Grochowski <grochowski0stanislaw@gmail.com>
2024-10-13 21:00:11 +0200

    image minature scraping

--- data_scraper/scraper_service.py
+++ data_scraper/scraper_service.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from lxml import etree
+import os
 
 
 def get_data(base_url: str, url_counter: int):
@@ -12,6 +13,13 @@ def get_data(base_url: str, url_counter: int):
         print(f"Error: {e}")
         return
 
+    image_dir = 'pictures'
+    try:
+        os.mkdir(image_dir)
+    except:
+        pass
+
+
     for product in main_div.find_all("div", {"class": "product-inner-wrap"}):
         if product is None:
             continue
@@ -20,8 +28,19 @@ def get_data(base_url: str, url_counter: int):
         product_basket = product.find("div", {"class": "product__basket"})
         product_price = product_basket.find("div", {"class": "price f-row"})
 
+        product_img = product.find('img')
+        source = 'https://motkomania.pl{}'.format(product_img['data-src'])  
+
         name = product_name.get_text(strip=True)
         price = product_price.get_text(strip=True)
         with open("data.txt", "a", encoding="utf8") as file:
             text = f"{name:60} {price.split(":")[1].strip()}"
             file.write(f"{text}\n")
+
+        flag = requests.get(source)
+        if flag.status_code != 200:
+            print("error")
+        else:
+            path = image_dir + '/' + name + '.jpg'
+            with open(path, 'wb') as f:
+                f.write(flag.content)
