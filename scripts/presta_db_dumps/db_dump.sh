4e8e392f9915e3244626050cc67300190842a289
Lucjan Gackowski <33768095+varrios@users.noreply.github.com>
2024-11-18 13:18:02 +0100

    Basic prestashop config implemented.  (#10)
    * Init prestashop
    
    * Testing prestashop init
    
    * Prestashop installed
    
    * Refactored file structure. Presta config done. Closes #5
    
    * Project structure refactor
    
    * Revert "Project structure refactor"
    
    This reverts commit 0222511bd2dcce2f2d79af3017ce6a5ae2d25959.
    
    ---------
    
    Co-authored-by: mikkelangelas <msugalski20@gmail.com>

---
+++ scripts/presta_db_dumps/db_dump.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Wczytanie zmiennych z pliku .env (jeśli istnieje)
+if [ -f ../.env ]; then
+  source ../.env
+fi
+
+# Ustawienia bazy danych
+DB_CONTAINER=ebiz-mysql
+DB_USER=${DB_USER}
+DB_PASSWD=${DB_PASSWD}
+DB_NAME=prestashop
+DUMP_FILE=prestashop_dump.sql
+
+# Sprawdzenie, czy zmienne środowiskowe DB_USER i DB_PASSWD są ustawione
+if [ -z "$DB_USER" ] || [ -z "$DB_PASSWD" ]; then
+  echo "Brak ustawionych zmiennych środowiskowych DB_USER lub DB_PASSWD."
+  exit 1
+fi
+
+# Wykonanie dumpa
+echo "Wykonywanie dumpa bazy danych '$DB_NAME'..."
+docker exec $DB_CONTAINER sh -c "exec mysqldump -u$DB_USER -p$DB_PASSWD $DB_NAME" > $DUMP_FILE
+
+if [ $? -eq 0 ]; then
+  echo "Dump zapisany w pliku $DUMP_FILE"
+else
+  echo "Wystąpił błąd podczas wykonywania dumpa bazy danych."
+  exit 1
+fi
---
+++ scripts/presta_db_dumps/db_load.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Wczytanie zmiennych z pliku .env (jeśli istnieje)
+if [ -f ../.env ]; then
+  source ../.env
+fi
+
+# Ustawienia bazy danych
+DB_CONTAINER=ebiz-mysql
+DB_USER=${DB_USER}
+DB_PASSWD=${DB_PASSWD}
+DB_NAME=prestashop
+DUMP_FILE=prestashop_dump.sql
+
+# Sprawdzenie, czy zmienne środowiskowe DB_USER i DB_PASSWD są ustawione
+if [ -z "$DB_USER" ] || [ -z "$DB_PASSWD" ]; then
+  echo "Brak ustawionych zmiennych środowiskowych DB_USER lub DB_PASSWD."
+  exit 1
+fi
+
+# Wykonanie dumpa
+echo "Ładowanie dumpa do bazy danych '$DB_NAME'..."
+docker exec $DB_CONTAINER sh -c "exec mysql -u$DB_USER -p$DB_PASSWD $DB_NAME" < $DUMP_FILE
+
+if [ $? -eq 0 ]; then
+  echo "Dump załadowany pomyślnie."
+else
+  echo "Wystąpił błąd podczas ładowania dumpa do bazy danych."
+  exit 1
+fi
